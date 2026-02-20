56c11a65fc92209794c5a345fa8f8b665169a179
utkarsh-kapoor-wq <kapoorutkarsh17@gmail.com>
2026-02-21 02:19:15 +0530

    add: initial project setup

---
+++ src/main/groovy/in/respoindly/auth/AuthApplication.groovy
@@ -0,0 +1,13 @@
+package in.respoindly.auth
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AuthApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AuthApplication, args)
+	}
+
+}
