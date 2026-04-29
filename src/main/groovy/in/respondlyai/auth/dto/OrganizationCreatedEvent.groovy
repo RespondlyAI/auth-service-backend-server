13ef64c3ebd2d7cdbce902e7af6fe980f8cc8489
utkarsh-kapoor-wq <kapoorutkarsh17@gmail.com>
2026-04-29 14:32:29 +0530

    feat: implemented organization created DTO

---
+++ src/main/groovy/in/respondlyai/auth/dto/OrganizationCreatedEvent.groovy
@@ -0,0 +1,17 @@
+package in.respondlyai.auth.dto
+
+import groovy.transform.Canonical
+import java.util.UUID
+
+@Canonical
+class OrganizationCreatedEvent {
+    UUID organizationId
+    UUID ownerUserId
+    List<InvitedUser> invitedUsers
+
+    @Canonical
+    static class InvitedUser {
+        String email
+        String role
+    }
+}
